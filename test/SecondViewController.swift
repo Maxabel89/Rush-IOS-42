a929225429fee86bffdb50576d9332b501ac2039
Maxime PORCHER <mporcher@e1r2p10.42.fr>
2015-11-25 15:58:34 +0100

    IOS

---
+++ test/SecondViewController.swift
@@ -0,0 +1,90 @@
+//
+//  SecondViewController.swift
+//  test
+//
+//  Created by swift on 20/12/14.
+//  Copyright (c) 2014 swift. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class SecondViewController: UIViewController, CLLocationManagerDelegate  {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let  locationManager = CLLocationManager()
+    @IBOutlet weak var bouton: UIButton!
+    var i = false;
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let location = CLLocationCoordinate2D(
+            latitude: 48.8965812,
+            longitude: 2.3183760
+        )
+        
+        let span = MKCoordinateSpanMake(0.003, 0.003)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Ecole 42"
+        annotation.subtitle = "Born to Code"
+        mapView.addAnnotation(annotation)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+
+    @IBAction func indexChanged(sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex{
+        case 0:
+            mapView.mapType = MKMapType.Satellite;
+        case 1:
+            mapView.mapType = MKMapType.Hybrid;
+        case 2:
+            mapView.mapType = MKMapType.Standard;
+        default:
+            mapView.mapType = MKMapType.Satellite;
+        }
+    }
+
+    
+    @IBAction func myButtonPressed(sender : UIButton){
+        if (i == false)
+        {
+            bouton.setImage(UIImage(named:"rsz_fleche1.png"),forState:UIControlState.Normal)
+            self.locationManager.delegate = self
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            self.locationManager.requestWhenInUseAuthorization()
+            self.locationManager.startUpdatingLocation()
+            i = true
+        }
+        else
+        {
+            bouton.setImage(UIImage(named:"Location.png"),forState:UIControlState.Normal)
+            self.locationManager.stopUpdatingLocation()
+            i = false
+        }
+    }
+ 
+    
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002))
+        
+        mapView.setRegion(region, animated: true)
+        }
+
+}
---
+++ test/ThirdViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ThirdViewController.swift
+//  test
+//
+//  Created by swift on 20/12/14.
+//  Copyright (c) 2014 swift. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ThirdViewController: UIViewController {
+    
+    let tableData = ["Paris","Ecole 42"," Silicon Valley"]
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int{
+            return self.tableData.count;
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    {
+        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.Default, reuseIdentifier:"cell")
+        cell.textLabel.text = tableData[indexPath.row]
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if (indexPath.row == 0)
+        {
+            let location = CLLocationCoordinate2D(
+                latitude: 48.8566140,
+                longitude: 2.3522219
+            )
+            
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location, span: span)
+            
+            //SecondViewController().mapView.setRegion(region, animated: true)
+        }
+        else if (indexPath.row == 1)
+        {
+                let location = CLLocationCoordinate2D(
+                    latitude: 48.8965812,
+                    longitude: 2.3183760
+                )
+                
+                let span = MKCoordinateSpanMake(0.05, 0.05)
+                let region = MKCoordinateRegion(center: location, span: span)
+                //SecondViewController().mapView.setRegion(region, animated: true)
+        }
+        else if (indexPath.row == 2)
+        {
+            let location = CLLocationCoordinate2D(
+                latitude: 37.7749295,
+                longitude: -122.4194155
+            )
+            
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location, span: span)
+            //SecondViewController().mapView.setRegion(region, animated: true)
+        }
+        
+    }
+}
